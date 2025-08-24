b4f9dc7bb12c9d162bc8ec24f173c6559f2716ec
zachaareee <zacharyjaptha13@gmail.com>
2025-08-24 17:06:08 +0200

    Medication Confirmation Screen + Styles Folder + Fonts folder

--- app/src/main/java/com/example/medmanage/activities/LoginActivity.java
+++ app/src/main/java/com/example/medmanage/activities/LoginActivity.java
@@ -35,7 +35,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.log_in);
 
         // Initialize UI components
         usernameEditText = findViewById(R.id.editTextTextPersonName);
